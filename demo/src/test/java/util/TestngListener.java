7d65fcee7dda034a6708bff1e9fb3018f2289178
liuxingtao666 <1457255937@.com>
2018-07-10 10:12:12 +0800

    修改黄色感叹号

--- demo/src/test/java/util/TestngListener.java
+++ demo/src/test/java/util/TestngListener.java
@@ -1,12 +1,6 @@
 package util;
 
-import java.io.File;
-import java.text.SimpleDateFormat;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 
